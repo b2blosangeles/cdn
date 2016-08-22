099e258af88b8b7afeb9ade80cd9280edc082dec
b2blosangeles <b2blosangeles@gmail.com>
2016-08-21 17:26:04 -0700

    Update testcase.js

--- test/testcase.js
+++ test/testcase.js
@@ -1,1 +1,20 @@
-res.send('kkk');
+res.send(env);
+var path = require('path');
+/*
+var Mocha = require(path.join(__dirname, '../../', 'package/mocha/node_modules/mocha'));
+
+var mocha = new Mocha;
+mocha.addFile('tt.js');
+mocha.reporter('json');
+
+var write = process.stdout.write;
+var output = '';
+process.stdout.write = function(str) {
+  output += str;
+};
+
+mocha.run(function(failures) {
+  process.stdout.write = write;
+  console.log(JSON.parse(output))
+});
+*/
