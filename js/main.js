7012ae83c3ce7b22be2cbafe990829dfcf0f6d8e
b2blosangeles <b2blosangeles@gmail.com>
2016-08-24 15:45:55 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,27 @@ var app = angular.module('qaletApp', [
 	'ngRoute'
 ]);
 app.controller('mainController', function($rootScope, $scope, $location, $http, $cookies, $timeout, $sce){ 
+	$scope.getVhostList = function(data) {
+		var list  = [];
+		for(var i=0; i< data.longth; i++) {
+			for ((k, v) in data[i]) {
+				if (list.indexOf(k) == -1) {
+					list[list.length] = k;	
+				}
+			}	
+		}
+		$scope.vhost_list = list;
+		
+		console.log($scope.vhost_list);
+	}
+	
+	
 	$scope.loadGitLog = function() {
 		$http({
 		  method: 'GET',
 		  url: '/api/git_log.js'
 		}).then(function successCallback(response) {
+			$scope.getVhostList(response.data);
 			console.log(response);
 		  }, function errorCallback(response) {	
 			console.log(response);						
