1dedbde14c43893e15d4a8f7f178634b4cdcb4a5
b2blosangeles <b2blosangeles@gmail.com>
2016-08-23 23:44:04 -0700

    Update git_log.js

--- api/git_log.js
+++ api/git_log.js
@@ -1,8 +1,10 @@
-pkg.db.git_log.find({}).sort({ created: -1 }).exec(function (err, docs) {
-	if (!err) {
-		res.send(docs)
-	} else {
-		res.send(err)
-	}
-	
-})
+(function(pkg, res) {
+	pkg.db.git_log.find({}).sort({ created: -1 }).exec(function (err, docs) {
+		if (!err) {
+			res.send(docs)
+		} else {
+			res.send(err)
+		}
+		
+	});
+})(pkg, res);
