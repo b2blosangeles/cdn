51e0e86ac241dc242e3ea2028b268cce5f765daa
b2blosangeles <b2blosangeles@gmail.com>
2016-08-24 07:51:10 -0700

    Update git_log.js

--- api/git_log.js
+++ api/git_log.js
@@ -1,4 +1,5 @@
 //(function(pkg, res) {
+res.send(env.root_space);
 var git_log =  new pkg.Nedb({ filename: env.root_path + '/_db/git_log.db', autoload: true });
 	git_log.find({}, { multi: true }).sort({ time: -1 }).exec(function (err, docs) {
 		if (!err) {
