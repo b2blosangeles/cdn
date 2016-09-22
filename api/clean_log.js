c0e99613705ad675284aa72e69d28dd325dd3a70
b2blosangeles <b2blosangeles@gmail.com>
2016-09-21 20:58:24 -0700

    Delete clean_log.js

--- api/clean_log.js
+++
@@ -1,6 +0,0 @@
-var exec = require('child_process').exec;
-(function(res) {
-  exec('truncate /var/log/qalet_cron.log --size 0 && truncate /var/log/qalet_node.log --size 0', function(error, stdout, stderr) {
-    res.send('done');
-  });
-})(res);
