2da54409d514fa3f507ca6f39e2dc2ecc5e6d935
b2blosangeles <b2blosangeles@gmail.com>
2016-09-25 00:11:21 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,5 @@
 var Busboy = require(env.root_path + '/package/busboy/node_modules/busboy');
 var socket_id = req.param('socket_id');
 
-io.to(socket_id).emit('messages', {code:'progress', n:socket_id});
+io.to(socket_id).emit('message', {code:'progress', n:socket_id});
 res.send(socket_id);
