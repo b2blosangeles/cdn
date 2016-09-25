d428a4d11c5e55a09f50761ebfd96b6ea6aa3984
b2blosangeles <b2blosangeles@gmail.com>
2016-09-25 06:46:13 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -3,29 +3,20 @@ var socket_id = '/#' + req.param('socket_id');
 var fs = require('fs');
 var busboy = new Busboy({ headers: req.headers });
 
-      busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
 
       file.pipe(fs.createWriteStream(env.space_path+'/videos/'+fieldname));
-
       file.on('data', function(data) {
-          //io.to(socket_id).emit('messages', 'File [' + fieldname + '] got ' + data.length + ' bytes');
           io.to(socket_id).emit('message', {code:'progress', bytes:data.length});
       });
+      
       file.on('end', function() {
-        res.send('File [' + fieldname + '] Finished');
+            io.to(socket_id).emit('message', 'done!!');
       });
     });
-    /*
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-    });
-*/
-    busboy.on('finish', function() {
-    //  console.log('Done parsing form!');
-     // res.writeHead(200, { Connection: 'close', Location: '/' });
-      res.end();
-    });
-    return req.pipe(busboy);
 
-//io.to(socket_id).emit('message', {code:'progress', n:socket_id});
-//res.send(socket_id);
+busboy.on('finish', function() {
+       io.to(socket_id).emit('message', 'done--!!');
+});
+// return req.pipe(busboy);
+res.send('Done!');
