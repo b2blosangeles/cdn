194e085020c62a052e5a8e016a212f6668bab2f2
b2blosangeles <b2blosangeles@gmail.com>
2016-09-25 00:18:33 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,30 @@
 var Busboy = require(env.root_path + '/package/busboy/node_modules/busboy');
 var socket_id = '/#' + req.param('socket_id');
 
-io.to(socket_id).emit('message', {code:'progress', n:socket_id});
-res.send(socket_id);
+var busboy = new Busboy({ headers: req.headers });
+busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+
+        file.pipe(fs.createWriteStream('niuC.mp4'));
+
+      file.on('data', function(data) {
+          //io.to(socket_id).emit('messages', 'File [' + fieldname + '] got ' + data.length + ' bytes');
+          io.to(socket_id).emit('messages', {code:'progress', bytes:data.length});
+      });
+      file.on('end', function() {
+        res.send('File [' + fieldname + '] Finished');
+      });
+    });
+    /*
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    });
+*/
+    busboy.on('finish', function() {
+    //  console.log('Done parsing form!');
+     // res.writeHead(200, { Connection: 'close', Location: '/' });
+      res.end();
+    });
+    return req.pipe(busboy);
+
+//io.to(socket_id).emit('message', {code:'progress', n:socket_id});
+//res.send(socket_id);
