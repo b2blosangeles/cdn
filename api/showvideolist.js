d0d09e8ca908abebb5c9e4afaa2f13e56a5204a5
b2blosangeles <b2blosangeles@gmail.com>
2016-09-25 17:43:11 -0700

    Update showvideolist.js

--- api/showvideolist.js
+++ api/showvideolist.js
@@ -2,9 +2,9 @@
   fs.readdir(env.space_path + '/videos', function(err, data) {
     var ret = [];
     for (var i=0; i < data.length; i++) {
-      if (data[i].test(/\.mp4$/i)) {
-        ret[ret.length] = data[i];
-      }
+      // if (data[i].test(/\.mp4$/i)) {
+       // ret[ret.length] = data[i];
+      //}
     }
     res.send(ret);
 
