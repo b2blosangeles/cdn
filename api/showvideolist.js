fe9b0e2c9cbbfb964e686ef9d5b1c7e184ff02ab
b2blosangeles <b2blosangeles@gmail.com>
2016-09-28 07:59:28 -0700

    Update showvideolist.js

--- api/showvideolist.js
+++ api/showvideolist.js
@@ -1,4 +1,5 @@
   var fs = require('fs');
+  var socket_id = '/#' + req.param('socket_id');
   fs.readdir(env.space_path + '/videos', function(err, data) {
     var ret = [];
     for (var i=0; i < data.length; i++) {
@@ -7,5 +8,5 @@
       }
     }
     res.send(ret);
-
+     io.to(socket_id).emit('message', {js:function() {alert('done')}});
   });
