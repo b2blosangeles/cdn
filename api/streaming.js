ed2c41c501903f420f6144625e50ff05f1f48e6d
b2blosangeles <b2blosangeles@gmail.com>
2016-10-05 07:54:36 -0700

    Update streaming.js

--- api/streaming.js
+++ api/streaming.js
@@ -7,20 +7,21 @@ var fn = env.space_path + '/videos/movie.mp4';
     else {
       var total = data.size;
      
-   //   if (req.headers['range']) {
+
         var range = req.headers.range;
-      res.send(range);
-      return true;
+
       
         var parts = range.replace(/bytes=/, "").split("-");
         var partialstart = parts[0];
         var partialend = parts[1];
-
+      
+        var partialstart = 0;
+        var partialend = 0;
+      
         var start = parseInt(partialstart, 10);
         var end = partialend ? parseInt(partialend, 10) : total-1;
         var chunksize = (end-start)+1;
-        console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
-
+    
         var file = fs.createReadStream(fn, {start: start, end: end});
         res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
         file.pipe(res);
