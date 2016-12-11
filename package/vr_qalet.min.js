601ef1b0085d5b70e82317f7852471e56ad66ede
b2blosangeles <b2blosangeles@gmail.com>
2016-12-10 22:21:38 -0800

    Update vr_qalet.min.js

--- package/vr_qalet.min.js
+++ package/vr_qalet.min.js
@@ -4,6 +4,7 @@
 			"angular-1.4.5/angular.min.js", 
 			"angular-1.4.5/angular-cookies.min.js", 
 			"angular-1.4.5/angular-route.min.js",
+			"js/moment.min.js",
 			"js/jquery.min.js", 
 			"js/bootstrap.min.js"			
  		]
